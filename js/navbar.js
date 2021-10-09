afc7168611dea6ee84e5e31fe30971c9aabefdcf
antac <angello_04_09@hotmail.com>
2021-10-09 15:01:02 -0500

    inicio terminado al 90%,falta js estilos de estado y ganancia transformal

--- js/navbar.js
+++ js/navbar.js
@@ -1,21 +1,4 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    /* verificarZise(desktop); */
-})
-
 const navbar = document.getElementById('navbar');
 function quitarNavbar(){
     navbar.classList.toggle('active');
 }
-
-
-function verificarZise(item){
-    if(item.matches){
-
-    }else{
-
-    }
-}
-
-let desktop = window.matchMedia("(max-width: 992px)");
-/* verificarZise(desktop); */
-desktop.addListener(verificarZise);
